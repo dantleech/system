3e9955770eb9a9d2ff61ae5b51d333c1055d0c93
Daniel <dan.t.leech@gmail.com>
2014-12-21 21:31:08 +0000

    Awesome

---
+++ roles/desktop/templates/awesome_variables.lua
@@ -0,0 +1,15 @@
+-- Themes define colours, icons, and wallpapers
+beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+
+-- This is used later as the default terminal and editor to run.
+terminal = "xfce4-terminal"
+editor = os.getenv("EDITOR") or "editor"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+modkey = "Mod4"
+
