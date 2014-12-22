681b90013ba9fc78953a206a4554c97b9d3b2037
Daniel <dan.t.leech@gmail.com>
2014-12-22 11:45:03 +0000

    Configured vimb as default browser

--- roles/mail/templates/mail-notify.sh
+++ roles/mail/templates/mail-notify.sh
@@ -8,26 +8,17 @@ mailboxes=("{{ user.mail.boxes|join('" "') }}") # Mailboxes to watch.
 watchdirs=$(for box in ${mailboxes[*]}; do echo ${maildir_path}/$box/new; done)
 
 scriptname=${0##*/}
-#multicut_file="/tmp/${scriptname}_${USER}_multipart_cut.txt"
-#trimmed_file="/tmp/${scriptname}_${USER}_trimmed.txt"
-
-trim() {
-    local var="$1"
-    var="${var#"${var%%[![:space:]]*}"}" # Remove leading whitespace characters.
-    var="${var%"${var##*[![:space:]]}"}" # Remove trailing whitespace characters.
-    echo -n "$var"
-}
 
 parse_send() {
     local inbox="$1"
     local mailfile="$2"
 
-    echo $mailfile
     subject=$(cat $2 | egrep '^Subject' | sed -e 's/^Subject://g')
     from=$(cat $2 | egrep '^From' | sed -e 's/^From://g')
 
     # Send the message with the name this scrip was invoked with.
-    notify-send -i /usr/share/icons/Adwaita/22x22/actions/mail-message-new.png ---app-name "$scriptname" "$inbox $from" "$subject"
+    export DISPLAY=:0; export XAUTHORITY={{ user.home }}/.Xauthority; 
+    notify-send -i /usr/share/icons/Adwaita/22x22/actions/mail-message-new.png "$inbox $from" "$subject"
 }
 
 # Debug with static file.
@@ -39,7 +30,6 @@ fi
 # Let inotifywait monitor changes and output each event on it's own line.
 while read mail; do
 
-echo "foo"
     # Split inotify output to get path and the file that was added.
     parts=($(echo "$mail" | sed -e 's/ \(CREATE\|MOVED_TO\) / /'))
     inbox_path="${parts[0]}"
