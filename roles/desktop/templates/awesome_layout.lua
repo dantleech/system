51881f03f3c0d90606476c06918645846d582368
dantleech <dan.t.leech@gmail.com>
2015-01-16 22:02:28 +0000

    Varnish and mpd

--- roles/desktop/templates/awesome_layout.lua
+++ roles/desktop/templates/awesome_layout.lua
@@ -1,11 +1,11 @@
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
+--    awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
+--    awful.layout.suit.tile,
 --    awful.layout.suit.fair,
 --    awful.layout.suit.fair.horizontal,
 --    awful.layout.suit.spiral,
